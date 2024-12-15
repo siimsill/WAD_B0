4bf090c6cc71dd3cd7ac2eddf9f78139bfaf86c7
MattiasAv <117313832+MattiasAv@users.noreply.github.com>
2024-12-15 20:59:42 +0200

    Revert "Merge branch 'main' of https://github.com/siimsill/WAD_B0"
    This reverts commit b4d0931f7ed52c0ab5ba904f09459c401ba2fcae, reversing
    changes made to fb682caea544f4841e478e4cbe4ed23d26887a9c.

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <template>
-    <AppHeader /> <!-- The header with navigation links -->
+  <nav>
+    <router-link to="/">Home</router-link> |
+<!--     <router-link to="/signup">SignUp</router-link> | -->
+<!--     <router-link to="/login">LogIn</router-link> | -->
+    <router-link to="/about">About</router-link>
+  </nav>
   <router-view />
-    <AppFooter /> <!-- The footer -->
 </template>
 
-<script>
-import AppFooter from './components/AppFooter.vue';
-import AppHeader from './components/AppHeader.vue';
-
-export default {
-  name: 'App',
-  components: {
-    AppFooter,
-    AppHeader
-  }
-}
-</script>
-
 <style>
-body {
-  margin: 0;
-  padding: 0;
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  box-sizing: border-box;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
