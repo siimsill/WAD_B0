dd0cdc6abea87909ed6ea3ed3a4dbe700bd9476b
siimsill <siim.sillaots@ut.ee>
2024-12-15 20:39:46 +0200

    Added contacts page and component for header and footer

--- src/router/index.js
+++ src/router/index.js
@@ -31,8 +31,8 @@ const routes = [{
         component: LogIn,
     },
     {
-        path: "/about",
-        name: "about",
+        path: "/contact",
+        name: "Contact",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
